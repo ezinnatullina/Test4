e632209ef719005a23ed5680ee6417a8dc85a35d
ezinnatullina <elvira.zinnatullina@aurea.com>
2019-07-02 15:15:38 +0500

    REV2

--- a/b/D.java
+++ a/b/D.java
@@ -1,14 +1,14 @@
 package a.b;
 
-public class C {
+public class D {
+
     public void x() {
         int abc = 10;
         int cde = abc * 5;
         abc = cde + 7;
-        abc = cde * 3;
     }
 
-    public void y (){
+    public void z(){
         int ab = 10;
         int cd = ab * 5;
         ab = cd + 7;
